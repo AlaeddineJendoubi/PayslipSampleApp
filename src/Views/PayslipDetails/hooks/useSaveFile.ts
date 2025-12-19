d0a16ab0a371ee584d1fb0218979a52b0a98773e
AlaeddineJendoubi <36207352+AlaeddineJendoubi@users.noreply.github.com>
2025-12-19 11:54:30 +0100

    create file saver hook to manage saving files

---
+++ src/Views/PayslipDetails/hooks/useSaveFile.ts
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import FileSaverManager from '../../../services/fileSaver';
+
+export const useSaveFile = (fileName: string) => {
+  const [savedPath, setSavedPath] = useState('');
+  const [error, setError] = useState<string>('');
+  const handleSaveBundledFile = async () => {
+    try {
+      const path = await FileSaverManager.saveBundledFile(fileName);
+      setSavedPath(path);
+    } catch (err) {
+      console.log('err', err);
+      setError(String(err));
+    }
+  };
+
+  return {
+    handleSaveBundledFile,
+    savedPath,
+    error,
+  };
+};
