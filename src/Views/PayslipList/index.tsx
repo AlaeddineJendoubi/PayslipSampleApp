d274dfab6672f30a8e29b55dc6ed6bc55960a063
AlaeddineJendoubi <36207352+AlaeddineJendoubi@users.noreply.github.com>
2025-12-19 09:41:34 +0100

    Create Payslips list (render list with redux data)

--- src/Views/PayslipList/index.tsx
+++ src/Views/PayslipList/index.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { PayslipItem } from './components/PayslipItem';
+import { Payslip } from '../../modules/payslips/types';
+import { NavigationProp } from '../../app/navigation/types';
+import { useManagePayslips } from './hooks/useManagePayslips';
+import { FlatList } from 'react-native';
 
-export const Paysliplist: React.FC = () => {
+export const PayslipsList: React.FC = () => {
+  const navigation = useNavigation<NavigationProp>();
+  const { data } = useManagePayslips();
+
+  const navigateToDetails = (payslip: Payslip) => {
+    navigation.navigate('PayslipsDetails', { payslip });
+  };
+  const renderPayslipItem = ({ item }: { item: Payslip }) => (
+    <PayslipItem payslipData={item} onPress={() => navigateToDetails(item)} />
+  );
+
   return (
     <>
-      <PayslipItem
-        payslipData={{
-          id: 'test1',
-          fromDate: '2025-01-01',
-          toDate: '2025-02-01',
-          file: 'image.png',
-        }}
+      <FlatList
+        data={data}
+        renderItem={renderPayslipItem}
+        keyExtractor={item => item.id}
       />
     </>
   );
