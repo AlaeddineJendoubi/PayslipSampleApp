795fb405a25329f79d4b6d850a3f63d9b1ec4668
AlaeddineJendoubi <36207352+AlaeddineJendoubi@users.noreply.github.com>
2025-12-26 00:05:36 +0100

    upadte app.test (redux mocks)

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -6,6 +6,45 @@ import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
 
+jest.mock('react-redux', () => ({
+  Provider: ({ children }: any) => children,
+  useDispatch: () => jest.fn(),
+  useSelector: () => jest.fn(),
+}));
+jest.mock('../src/app/state/store', () => ({
+  store: {
+    getState: jest.fn(),
+    dispatch: jest.fn(),
+    subscribe: jest.fn(),
+  },
+}));
+jest.mock('@react-navigation/native', () => ({
+  NavigationContainer: ({ children }: any) => children,
+  DefaultTheme: {},
+  useNavigation: () => ({ navigate: jest.fn(), goBack: jest.fn() }),
+}));
+
+jest.mock('@react-navigation/native-stack', () => ({
+  createNativeStackNavigator: jest.fn(() => ({
+    Navigator: ({ children }: any) => children,
+    Screen: ({ children }: any) => children,
+  })),
+}));
+
+jest.mock('react-native-feather', () => {
+  const React = require('react');
+  const { View } = require('react-native');
+  const MockIcon = () => <View />;
+  return {
+    Activity: MockIcon,
+    Hash: MockIcon,
+    Calendar: MockIcon,
+    ArrowRight: MockIcon,
+    FileText: MockIcon,
+    Image: MockIcon,
+  };
+});
+
 test('renders correctly', async () => {
   await ReactTestRenderer.act(() => {
     ReactTestRenderer.create(<App />);
